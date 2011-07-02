e00a872994b37e6b504300f83ada543ad53df267
Cary Chow <carychowhk@gmail.com>
2011-07-02 21:33:08 +0800

    Updated the example.

--- example.php
+++ example.php
@@ -2,28 +2,63 @@
 // Include the main.php then all the files of EternalPlurk will be included.
 require_once('src/main.php');
 
-$apiKey = 'Your API key';
+$consumerKey = 'Plurk App\'s key.';
+$consumerSecret = 'The Plurk App\'s secret.';
+$oAuthToken = null;
+$oAuthTokenSecret = null;
 
-// Create a PlurkApp instance.
-$app = new PlurkApp();
-
-// Setup the path of Cookie file.
-$app->setCookiePath(dirname(__FILE__) . '/cookie.dat');
-
-// Setup API key to access Plurk API
-$app->setApiKey($apiKey);
-
-// In this example, try to get the public profile of me (carychow).
-$info = $app->getPublicProfile('carychow');
-
-if($info === false)
+if(empty($oAuthToken) && empty($oAuthTokenSecret))
 {
-	// Failed. Print the error message.
-	echo $app->getErrMsg();
+	// Need to authorization
+	
+	// Create a PlurkApp instance.
+	$app = new PlurkApp($consumerKey, $consumerSecret);
+	
+	if(empty($_GET['oauth_token']) && empty($_GET['oauth_verifier']))
+	{
+		// It's not a callback.
+		
+		// In the authorization, it will redirect to Plurk's authorization page.
+		if(!$app->startAuth())
+		{
+			// If there is error, print the error message.
+			echo $app->getErrMsg();
+		}
+	}
+	else
+	{
+		$info = $app->parseCallback();
+		
+		if($info === false)
+		{
+			// Failed. Print the error message.
+			echo $app->getErrMsg();
+		}
+		else
+		{
+			// Got a PlurkOAuthInfo object.
+			// Please use $info->oAuthToken and $info->oAuthToken to accees Plurk API for the user(s).
+			var_dump($info);
+		}
+	}
 }
 else
 {
-	// Got a PlurkProfileInfo object.
-	var_dump($info);
+	// Create a PlurkApp instance.
+	$app = new PlurkApp($consumerKey, $consumerSecret, $oAuthToken, $oAuthTokenSecret);
+	
+	// Try to get the public profile of me (carychow).
+	$info = $app->getPublicProfile('carychow');
+	
+	if($info === false)
+	{
+		// Failed. Print the error message.
+		echo $app->getErrMsg();
+	}
+	else
+	{
+		// Got a PlurkProfileInfo object.
+		var_dump($info);
+	}
 }
 ?>
